877e00d1c6b9f603192fc58fd85fd51a10d4e218
fahadawan16 <fahadkamra8@gmail.com>
2025-05-28 22:07:56 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  author: 'FAHAD‎',
+  botName: "LEO TECH 🤠",
+  botOwner: 'FAHAD AWAN', // Your name
+  ownerNumber: '923276096349', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
